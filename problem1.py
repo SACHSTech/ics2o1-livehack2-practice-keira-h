6201859dfe16a97503c4cfb53b4642bbe7dacf97
Keira Hosey <keira.hosey23@ycdsbk12.ca>
2021-02-12 15:43:45 +0000

    touch ups

--- problem1.py
+++ problem1.py
@@ -5,7 +5,7 @@ Purpose: Write a program that asks the user to enter three           values and 
 
 Author:  Hosey.K
 
-Created: date in 11/02/2021
+Created: date in 12/02/2021
 ----------------------------------------------------------
 """
 
@@ -20,7 +20,7 @@ squared_2 = side_2**2
 squared_3 = side_3**2
 
 # Compute Results
-if squared_1 + squared_2 == squared_3 or squared_2 + squared_3 == squared_1 or squared_1 + squared_3 == squared_2:
+if (squared_1 + squared_2 == squared_3) or (squared_2 + squared_3 == squared_1) or (squared_1 + squared_3 == squared_2):
   print("The triangle with side lengths " + str(side_1) + ", " + str(side_2) + ", and " + str(side_3) + " form a right-angled traingle.")
 else:
   print("The triangle with side lengths " + str(side_1) + ", " + str(side_2) + ", and " + str(side_3) + " do not form a right-angled traingle.")
--- problem2.py
+++ problem2.py
@@ -5,18 +5,18 @@ Purpose: Write a program that determines whether the user can go on a summer vac
 
 Author:  Hosey.K
 
-Created: date in 11/02/2021
+Created: date in 12/02/2021
 ----------------------------------------------------------
 """
 
 # Get the information
-grade = int(input("Enter your averaged (%): "))
-earnings = int(input("Enter the the earnings: "))
+grade = float(input("Enter your averaged (%): "))
+earnings = float(input("Enter the the earnings: "))
 
 # Compute Results
-if grade > 80 and earnings > 500:
+if grade >= 80 and earnings >= 500:
   print("Congratulations! You get to go on a trip to Europe for your summer vacation.")
-elif grade > 80 and earnings < 500:
+elif grade >= 80 and earnings <= 500:
   print("Congratulations! You get to go on a trip to California for your summer vacation.")
 else:
   print("Sorry! You do not get a summer vacation. There's always next time!")
